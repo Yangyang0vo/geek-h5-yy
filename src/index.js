73aafe8b6593b12b6ac68da943898e9040300cfe
洋洋 <2372542263@qq.com>
2022-09-03 21:52:57 +0800

    项目搭建-通用样式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // @ts-nocheck
 import App from './App'
 import React from 'react'
+// 导入通用样式
+import './assets/styles/index.scss'
 import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(<App></App>)
