0443c4b77e1bdf882380fb3cdec0e5c149ba192c
洋洋 <2372542263@qq.com>
2022-09-06 14:22:20 +0800

    项目搭建-整体路由布局以及各个组件

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // @ts-nocheck
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import React, { Suspense } from 'react'
+// 路由懒加载
 const Login = React.lazy(() => import('@/pages/Login'))
-const Home = React.lazy(() => import('@/pages/Home'))
+const TabBarLayout = React.lazy(() => import('@/layouts/TabBarLayout'))
 export default function App() {
   return (
     <Router>
       <div className="app">
         <Suspense fallback={<div>loading...</div>}>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<Home />}></Route>
+            <Route path="/home/*" element={<TabBarLayout />} />
+            {/* <Route path="*" element={<TabBarLayout />}></Route> */}
             <Route path="/login" element={<Login />}></Route>
-            <Route path="/home" element={<Home />}></Route>
           </Routes>
         </Suspense>
       </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Icon from '@/components/Icon'
+import styles from './index.module.scss'
 export default function Home() {
   return (
-    <div>
-      <div>Home666</div>
-      <Icon type="icon-icon-fanhui" style={{ fontSize: 110 }} onClick={() => alert('666')}></Icon>
+    <div className={styles.root}>
+      {/* 顶部工具栏 */}
+      {/* 后退按钮 */}
+      <div className="left">
+        <Icon type="icon-icon-fanhui"></Icon>
+      </div>
+      {/* 居中标题 */}
+      <div className="title">我是标题</div>
+      {/* 右侧内容 */}
+      <div className="right">我是内容</div>
     </div>
   )
 }
