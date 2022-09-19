cd58c0465c1ce33fa64e4b1e20d7d29186782ce2
洋洋 <2372542263@qq.com>
2022-09-20 01:43:36 +0800

    ts改造-img组件

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -3,8 +3,13 @@ import Icon from '../Icon'
 import styles from './index.module.scss'
 import { useEffect, useState } from 'react'
 import { useRef } from 'react'
-const Image = ({ className, src, alt }) => {
-  const imgRef = useRef(null)
+type Props = {
+  src: string
+  alt?: string
+  className?: string
+}
+const Image = ({ className, src, alt }: Props) => {
+  const imgRef = useRef<HTMLImageElement>(null)
   // 控制是否加载中
   const [loading, setLoading] = useState(true)
   // 控制是否加载失败
@@ -19,16 +24,17 @@ const Image = ({ className, src, alt }) => {
   }
   useEffect(() => {
     // 监听图片
+    const current = imgRef.current!
     const observer = new IntersectionObserver(([{ isIntersecting }]) => {
       if (isIntersecting) {
         // 图片在可视区
-        imgRef.current.src = imgRef.current.dataset.src
+        current.src = current.dataset.src!
         // 取消监听
-        observer.unobserve(imgRef.current)
+        observer.unobserve(current)
       }
     })
     // 开始监听
-    observer.observe(imgRef.current)
+    observer.observe(current)
   }, [])
 
   return (
