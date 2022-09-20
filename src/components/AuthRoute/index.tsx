3a2388a44f5b1282785dd84e7977cee842cd59e2
洋洋 <2372542263@qq.com>
2022-09-20 19:01:14 +0800

    ts改造-AuthRoute改造

--- src/components/AuthRoute/index.tsx
+++ src/components/AuthRoute/index.tsx
@@ -1,8 +1,10 @@
 import { hasToken } from '@/utils/storage'
 
 import { Navigate, useLocation } from 'react-router-dom'
-
-export default function AuthRoute({ children }) {
+type Props = {
+  children: JSX.Element
+}
+export default function AuthRoute({ children }: Props) {
   const location = useLocation()
   const render = () => {
     // 判断是否有token 有就放行  没有导航到登录
